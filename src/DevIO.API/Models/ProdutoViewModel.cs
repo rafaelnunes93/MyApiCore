eb0aed7367c6313818c4a32de71c38e2385cbc4b
Rafael Nunes <rafaelnunes_93@hotmail.com>
2020-11-01 14:20:31 -0300

    Setup -Controller and Startup

---
+++ src/DevIO.API/Models/ProdutoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.API.ViewModels
+{
+    public class ProdutoViewModel
+    {
+    }
+}
