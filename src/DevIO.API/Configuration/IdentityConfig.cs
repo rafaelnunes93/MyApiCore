036afc90c2cc758c7e9d064715be4837285f86d5
Rafael Nunes <rafaelnunes_93@hotmail.com>
2020-11-02 15:27:05 -0300

    Customizando os erros do Identity

--- src/DevIO.API/Configuration/IdentityConfig.cs
+++ src/DevIO.API/Configuration/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.API.Data;
+using DevIO.API.Extensions;
 using DevIO.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,7 @@ namespace DevIO.API.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
 
             return services;
